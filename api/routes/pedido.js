dc3545bf30fd0c144b2f811e099bd7d6dac05cb6
Felipe Morales <felipe.morales.querol@gmail.com>
2022-01-23 20:29:29 -0300

    Fix routes/pedido.js remove swagger inline

--- api/routes/pedido.js
+++ api/routes/pedido.js
@@ -7,120 +7,16 @@ const pedidos = require("../models//pedidos");
 const authController = require("../controllers/authController");
 const pedidoController = require("../controllers/pedidoController");
 
-/**
- * @swagger
- * /api/pedidos:
- *  get:
- *    tags: [pedidos]
- *    summary: pedidos
- *    description: Listado de pedidos
- *    parameters:
- *       - in: query
- *         name: index
- *         required: true
- *         description: Index del usuario logueado.
- *         schema:
- *           type: integer
- *           example: -1
- *    responses:
- *       200:
- *         description: Listado de usuarios
- */
 router.get("/api/pedidos", authController.authenticated, pedidoController.get);
 
-/**
- * @swagger
- * /api/pedidos:
- *  post:
- *    tags: [pedidos]
- *    summary: Agregado de pedido.
- *    description : Agregado de pedido.
- *    consumes:
- *      - application/json
- *    parameters:
- *      - in: query
- *        name: index
- *        required: true
- *        description: Index del usuario logueado.
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: body
- *        name: pedido
- *        description: producto a crear
- *        schema:
- *          type: object
- *          required:
- *            - formaDePago
- *          properties:
- *            direccionEnvio:
- *              description: Dirección de envio
- *              type: string
- *              example:
- *            formaDePago:
- *              description: Forma de Pago (EF, TC, TD, MP)
- *              type: string
- *              example: EF
- *    responses:
- *      201:
- *       description: Pedido creado
- *      401:
- *       description: Pedido no creado
- *
- */
+
 router.post(
   "/api/pedidos",
   authController.authenticated,
   pedidoController.post
 );
 
-/**
- * @swagger
- * /api/pedidos/{id}:
- *  put:
- *    tags: [pedidos]
- *    summary: Modificación de pedido.
- *    description : Modificación de pedido.
- *    consumes:
- *      - application/json
- *    parameters:
- *      - in: query
- *        name: index
- *        required: true
- *        description: Index del usuario logueado.
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: path
- *        name: id
- *        required: true
- *        description: ID del pedido a modificar
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: body
- *        name: pedido
- *        description: producto a crear
- *        schema:
- *          type: object
- *          required:
- *            - formaDePago
- *          properties:
- *            direccionEnvio:
- *              description: Dirección de envio
- *              type: string
- *              example: "Calle x"
- *            formaDePago:
- *              description: Forma de Pago (EF, TC, TD, MP)
- *              type: string
- *              example: EF
- *    responses:
- *      201:
- *       description: Pedido modificado
- *      401:
- *       description: Pedido no modificado debido a error
- *
- */
+
 router.put(
   "/api/pedidos/:id",
   authController.authenticated,
@@ -157,32 +53,7 @@ router.put(
   }
 );
 
-/**
- * @swagger
- * /api/pedidos/{id}:
- *  get:
- *    tags: [pedidos]
- *    summary: pedidos según id pedido
- *    description: Listado de pedidos
- *    parameters:
- *       - in: query
- *         name: index
- *         required: true
- *         description: Index del usuario logueado.
- *         schema:
- *           type: integer
- *           example: -1
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID del pedido a mostrar
- *         schema:
- *           type: integer
- *           example: -1
- *    responses:
- *       200:
- *         description: Listado de usuarios
- */
+
 router.get(
   "/api/pedidos/:id",
   authController.authenticated,
@@ -203,93 +74,15 @@ router.get(
   }
 );
 
-/**
- * @swagger
- * /api/pedidos/{id}/producto/{codeProducto}:
- *  post:
- *    tags: [pedidos]
- *    summary: pedidos según id pedido
- *    description: Listado de pedidos
- *    consumes:
- *      - application/json
- *    parameters:
- *      - in: query
- *        name: index
- *        required: true
- *        description: Index del usuario logueado.
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: path
- *        name: id
- *        required: true
- *        description: ID del pedido a mostrar
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: body
- *        name: producto
- *        description: Código del producto a agregar al pedido
- *        schema:
- *          type: object
- *          required:
- *            - codeProducto
- *          properties:
- *            codeProducto:
- *              description: Código del producto
- *              type: string
- *              example: XX
- *    responses:
- *      200:
- *        description: Ok de producto agregado
- */
+
 router.post(
   "/api/pedidos/:id/producto/:codeProducto",
   authController.authenticated,
   pedidoController.postProducto
-  
+
 );
 
-/**
- * @swagger
- * /api/pedidos/{id}/estado/{codeEstado}:
- *  patch:
- *    tags: [pedidos]
- *    summary: Cambio de estado
- *    description: Cambio de estado de pedido vía id pedido
- *    consumes:
- *      - application/json
- *    parameters:
- *      - in: query
- *        name: index
- *        required: true
- *        description: Index del usuario logueado.
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: path
- *        name: id
- *        required: true
- *        description: ID del pedido a mostrar
- *        schema:
- *          type: integer
- *          example: -1
- *      - in: body
- *        name: codeEstado
- *        description: Código del estado a cambiar
- *        schema:
- *          type: object
- *          required:
- *            - codeEstado
- *          properties:
- *            codeEstado:
- *              description: Código de Estado (PEN, CON, ENP, ENV, ENT)
- *              type: string
- *              example: XX
- *    responses:
- *      200:
- *        description: Ok de producto agregado
- */
+
 router.patch(
   "/api/pedidos/:id/estado/:codeEstado",
   authController.authenticated,
