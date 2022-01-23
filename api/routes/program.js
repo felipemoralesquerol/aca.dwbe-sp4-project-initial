4792cd778db9591f21ad716019da7a627faaa758
Felipe Morales <felipe.morales.querol@gmail.com>
2022-01-23 20:30:30 -0300

    Fix program.js

--- api/routes/program.js
+++ api/routes/program.js
@@ -1,20 +1,6 @@
 const express = require("express");
 let router = express.Router();
-//const init = require("./../models/init");
 
-/**
- * @swagger
- * /:
- *  get:
- *    description: Referencia al programa especial
- *    tags: [generales]
- *    summary: programa
- *    responses:
- *      200:
- *        description: Descripción de la versión
- *      304:
- *        description: Descripción de la versión
- */
 router.get("/", function (req, res) {
   res.send({ programa: "Resto v2.0.0" });
 });
